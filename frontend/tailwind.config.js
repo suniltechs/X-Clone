fa8d5d45ff4f3905d08de481c8c83ebb44bf3c1b
suniltechs <suniltechs.ai@gmail.com>
2025-06-11 20:16:03 +0530

    initial commit

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,28 @@
+import daisyui from "daisyui";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      "light",
+      {
+        black: {
+          primary: "rgb(29, 155, 240)",
+          secondary: "rgb(24, 24, 24)",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#000000",
+          info: "#3abff8",
+          success: "#36d399",
+          warning: "#fbbd23",
+          error: "#f87272",
+        },
+      },
+    ],
+  },
+};
